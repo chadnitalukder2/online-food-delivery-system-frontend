3fdb95341b7960e3840e18e4d6f86e0cf4e38f83
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-10-30 02:26:57 +0600

    done restaurant section

--- src/views/Frontend/Home_page.vue
+++ src/views/Frontend/Home_page.vue
@@ -133,8 +133,161 @@
                     <h1>Popular Restaurant</h1>
                 </div>
                 <div class="all_restaurant">
-                    <a href="#">View All  <i class="fa-regular fa-circle-right"></i></a>
+                    <a href="#">View All <i class="fa-regular fa-circle-right"></i></a>
                 </div>
+            </div>
+            <div class="nearby-restaurant">
+                <div class="restaurant_item">
+                    <div class="restaurant__thumb">
+                        <div class="image">
+                            <a href="#">
+                                <img src="../../assets/restaurant-1.png">
+                            </a>
+                        </div>
+                        <div class="wishlist">
+                            <i class="fa-regular fa-heart"></i>
+                        </div>
+                    </div>
+                    <div class="restaurant__info">
+                        <div class="restaurant__info-thumb">
+                            <a href="#">
+                                <img src="../../assets/res-logo-1.png">
+                            </a>
+                        </div>
+                        <div class="restaurant__info-content">
+                            <h1 class="restaurant__info-content-title">
+                                <a href="#">Garden Fresh</a>
+                            </h1>
+                            <span class="restaurant__info-content-location">
+                                <i class="fa-regular fa-compass"></i> Miami, Florida
+                            </span>
+                            <div class="restaurant__info-content-review">
+                                <span><i class="fa-solid fa-star"></i></span> <span>(5.0)</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="restaurant_item">
+                    <div class="restaurant__thumb">
+                        <div class="image">
+                            <a href="#">
+                                <img src="../../assets/restaurant-1.png">
+                            </a>
+                        </div>
+                        <div class="wishlist">
+                            <i class="fa-regular fa-heart"></i>
+                        </div>
+                    </div>
+                    <div class="restaurant__info">
+                        <div class="restaurant__info-thumb">
+                            <a href="#">
+                                <img src="../../assets/res-logo-1.png">
+                            </a>
+                        </div>
+                        <div class="restaurant__info-content">
+                            <h1 class="restaurant__info-content-title">
+                                <a href="#">Garden Fresh</a>
+                            </h1>
+                            <span class="restaurant__info-content-location">
+                                <i class="fa-regular fa-compass"></i> Miami, Florida
+                            </span>
+                            <div class="restaurant__info-content-review">
+                                <span><i class="fa-solid fa-star"></i></span> <span>(5.0)</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="restaurant_item">
+                    <div class="restaurant__thumb">
+                        <div class="image">
+                            <a href="#">
+                                <img src="../../assets/restaurant-1.png">
+                            </a>
+                        </div>
+                        <div class="wishlist">
+                            <i class="fa-regular fa-heart"></i>
+                        </div>
+                    </div>
+                    <div class="restaurant__info">
+                        <div class="restaurant__info-thumb">
+                            <a href="#">
+                                <img src="../../assets/res-logo-1.png">
+                            </a>
+                        </div>
+                        <div class="restaurant__info-content">
+                            <h1 class="restaurant__info-content-title">
+                                <a href="#">Garden Fresh</a>
+                            </h1>
+                            <span class="restaurant__info-content-location">
+                                <i class="fa-regular fa-compass"></i> Miami, Florida
+                            </span>
+                            <div class="restaurant__info-content-review">
+                                <span><i class="fa-solid fa-star"></i></span> <span>(5.0)</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="restaurant_item">
+                    <div class="restaurant__thumb">
+                        <div class="image">
+                            <a href="#">
+                                <img src="../../assets/restaurant-1.png">
+                            </a>
+                        </div>
+                        <div class="wishlist">
+                            <i class="fa-regular fa-heart"></i>
+                        </div>
+                    </div>
+                    <div class="restaurant__info">
+                        <div class="restaurant__info-thumb">
+                            <a href="#">
+                                <img src="../../assets/res-logo-1.png">
+                            </a>
+                        </div>
+                        <div class="restaurant__info-content">
+                            <h1 class="restaurant__info-content-title">
+                                <a href="#">Garden Fresh</a>
+                            </h1>
+                            <span class="restaurant__info-content-location">
+                                <i class="fa-regular fa-compass"></i> Miami, Florida
+                            </span>
+                            <div class="restaurant__info-content-review">
+                                <span><i class="fa-solid fa-star"></i></span> <span>(5.0)</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="restaurant_item">
+                    <div class="restaurant__thumb">
+                        <div class="image">
+                            <a href="#">
+                                <img src="../../assets/restaurant-1.png">
+                            </a>
+                        </div>
+                        <div class="wishlist">
+                            <i class="fa-regular fa-heart"></i>
+                        </div>
+                    </div>
+                    <div class="restaurant__info">
+                        <div class="restaurant__info-thumb">
+                            <a href="#">
+                                <img src="../../assets/res-logo-1.png">
+                            </a>
+                        </div>
+                        <div class="restaurant__info-content">
+                            <h1 class="restaurant__info-content-title">
+                                <a href="#">Garden Fresh</a>
+                            </h1>
+                            <span class="restaurant__info-content-location">
+                                <i class="fa-regular fa-compass"></i> Miami, Florida
+                            </span>
+                            <div class="restaurant__info-content-review">
+                                <span><i class="fa-solid fa-star"></i></span> <span>(5.0)</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
@@ -364,6 +517,7 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
+            padding-bottom: 30px;
 
             .section-title {
                 h1 {
@@ -383,15 +537,139 @@
                     border-radius: 6px;
                     position: relative;
                     background: #9c4202;
-                    border: 1px solid ;
+                    border: 1px solid;
                     color: #fff;
                     text-decoration: none;
                     outline: none !important;
-                    &:hover{
-                        
+                    transition: all .3s;
+
+                    &:hover {
+                        transform: scale(1.1);
+                        background: #b54d02;
                     }
                 }
             }
+        }
+
+        .nearby-restaurant {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            gap: 30px;
+
+            .restaurant_item {
+                .restaurant__thumb {
+                    position: relative;
+                    transition: all ease 0.3s;
+                    overflow: hidden;
+                    border-radius: 8px 8px 0px 0px;
+
+                    .image {
+                        transition: all ease 0.3s;
+                        width: 100%;
+                        height: 175px;
+                        display: block;
+
+                        img {
+                            width: 100%;
+                            height: 100%;
+                        }
+
+                        &:hover {
+                            transform: scale(1.1);
+                        }
+
+
+                    }
+
+                    .wishlist {
+                        position: absolute;
+                        font-size: 20px;
+                        margin: 0 auto;
+                        top: 0;
+                        right: 0;
+                        background: #fff;
+                        color: #8f8787;
+                        height: 40px;
+                        width: 40px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        text-align: center;
+                        margin-right: 15px;
+                        margin-top: 12px;
+                        border-radius: 50%;
+                        box-shadow: 0px 0px 8px #ddd;
+                    }
+
+
+                }
+
+                .restaurant__info {
+                    padding: 25px 15px 20px 15px;
+                    background-color: #fff;
+                    border-radius: 0px 0px 8px 8px;
+                    display: flex;
+                    justify-content: space-between;
+
+                    .restaurant__info-thumb {
+                        margin-right: 15px;
+                        width: 45px;
+
+                        img {
+                            width: 43px;
+                            height: 40px;
+                            border-radius: 6px;
+                        }
+                    }
+
+                    .restaurant__info-content {
+                        width: calc(100% - 45px);
+
+                        .restaurant__info-content-title {
+                            line-height: 1;
+                            margin-bottom: 6px;
+
+                            a {
+                                text-decoration: none;
+                                font-size: 18px;
+                                font-weight: 600;
+                                color: #212121;
+                                transition: all .3s;
+                                &:hover{
+                                    color: #9c4202;
+                                }
+                            }
+                        }
+
+                        .restaurant__info-content-location {
+                            font-size: 16px;
+                            color: #999999;
+
+                            i {
+                                color: #9c4202;
+                                font-size: 16px;
+                                margin-right: 5px;
+                            }
+                        }
+
+                        .restaurant__info-content-review {
+                            margin-top: 6px;
+                            font-size: 13px;
+                            font-weight: 500;
+                            color: #8f8787;
+
+                            i {
+                                color: gold;
+                            }
+                        }
+                    }
+                }
+                &:hover{
+                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+                }
+            }
+
+
         }
     }
 }
