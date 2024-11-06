a2bd5965725d58635d7c760a61cfac13119d9dab
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-11-06 11:21:20 +0600

    wip:: menu page

--- src/views/Frontend/Menu/Menu_item.vue
+++ src/views/Frontend/Menu/Menu_item.vue
@@ -14,6 +14,39 @@
             </div>
         </div>
 
+        <section class="restaurant-page">
+            <div class="restaurant-info">
+                <div class="restaurant-info-box__thumb">
+                    <div class="image">
+                        <img src="../../../assets/res-logo-1.png">
+                    </div>
+                </div>
+                <div class="restaurant-info-box__meta">
+                    <h4 class="restaurant-info-box__meta-title">
+                        Urban Eats
+                    </h4>
+                    <span class="restaurant-info-box__meta-location"><i class="fas fa-map-marker-alt"></i>San Francisco,
+                        California</span>
+                    <ul class="restaurant-info-box__meta-rating">
+                        <li>
+                            <i class="las la-star active"></i><i class="las la-star active"></i><i
+                                class="las la-star active"></i><i class="las la-star active"></i><i
+                                class="las la-star active"></i>
+                        </li>
+
+                        <li><span class="badge badge--base badge--sm">OPEN</span></li>
+                        <li class="fav-icon " data-url="https://script.viserlab.com/hungryhub/restaurant/favorite/4"><i
+                                class="far fa-heart"></i></li>
+                    </ul>
+                    <ul class="restaurant-info-box__meta-time">
+                        <li><i class="las la-clock"></i>36 Mins </li>
+                        <li><i class="las la-biking"></i>$6.18 USD
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </section>
 
     </div>
 
@@ -56,6 +89,31 @@
         }
     }
 
+    .restaurant-page {
+        padding: 64px 80px;
+        background-color: #fff;
+        .restaurant-info {
+            display: flex;
+            gap: 30px;
+            .restaurant-info-box__thumb {
+                .image {
+                    width: 90px;
+                    height: 90px;
+                    border-radius: 6px;
+                    overflow: hidden;
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        object-fit: cover;
+                    }
+                }
+            }
+
+            .restaurant-info-box__meta {}
+
+
+        }
+    }
 
 }
 </style>
